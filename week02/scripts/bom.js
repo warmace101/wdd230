ecacb408b0458194794a24b33d6f8b908291b344
Johnathan Babb <johnathanwbabb@gmail.com>
2024-02-04 01:16:54 -0700

    hwupdate

---
+++ week02/scripts/bom.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+function addFavChap(){
+    var favChap = document.getElementById("favChap").ariaValueMax;
+    if (newScript !== ""){
+        var listItem = document.createElement("li");
+        listItem.textContent = favChap;
+        document.getElementById("list").appendChild(listItem);
+        document.getElementById("favChap").value = "";
+    }
+    else {
+        alert("Please enter a scripture before attempting to add one")
+    }
+}
