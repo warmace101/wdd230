873e556f91e5facb0e3b44f3642a1964d64e550c
Johnathan Babb <johnathanwbabb@gmail.com>
2024-02-07 21:54:52 -0700

    hwup

--- scripts/timesVisited.js
+++ scripts/timesVisited.js
@@ -1,24 +1,3 @@
-const input=document.querySelector('#favchap');
-const button=document.querySelector('button');
-const list=document.querySelector('#list');
-
-button.addEventListener('click', function() {
-    if (input.value != '') {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
-        deleteButton.addEventListener('click', function () {
-            list.removeChild(li);
-            input.focus();
-        });
-        input.focus();
-        input.value = '';
-    }
-});
-
 // 1️⃣ Initialize display element variable
 const visitsDisplay = document.querySelector(".visits");
 
